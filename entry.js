ef6a35f143d789884fd4a7b2a4794a53e9c6aff5
Michelle Zeng <mzeng@atlassian.com>
2016-01-31 15:26:40 +1100

    show one todo item on the board.

--- entry.js
+++ entry.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import data from "./example_data.js";
 const test="hello";
 console.log(test);
-//console.log(data);
+console.log(data);
 //
 //const element = <span>This is a span</span> ;
 //console.log(element);
 
 class TodoList extends Component{
     render(){
-        return <div>Hello Matt</div>;
+        return <p>{this.props.summary}</p> ;
     }
-};
+}
 
-ReactDOM.render(<TodoList/>, document.getElementById("app"));
+ReactDOM.render(<TodoList summary={data.summary} />, document.getElementById("app"));
--- example_data.js
+++ example_data.js
@@ -1,20 +1,4 @@
 export default {
-    "tasks": [
-        {
-            "summary": "learn ES6",
-            "status": "todo"
-        },
-        {
-            "summary": "learn npm",
-            "status": "todo"
-        },
-        {
-            "summary": "learn webpack",
-            "status": "todo"
-        },
-        {
-            "summary": "learn react",
-            "status": "todo"
-        }
-    ]
+    "summary": "learn ES6",
+    "status": "todo"
 }
