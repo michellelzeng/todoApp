a7374379b1656dc5d37fbf65116446b3a684af84
Michelle Zeng <mzeng@atlassian.com>
2016-02-01 09:15:43 +1100

    Call the render method only when dom is loaded.

--- entry.js
+++ entry.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import data from "./example_data";
 import {TodoList} from "./TodoList"
 
+const init = function() {
+    ReactDOM.render(<TodoList list={data.items}/>, document.getElementById("app"));
+};
 
-ReactDOM.render(<TodoList list={data.items}/>, document.getElementById("app"));
+document.addEventListener('DOMContentLoaded', init);
