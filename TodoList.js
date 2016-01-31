28b4e3c201f99f962e76a170f998eb560156d33e
Michelle Zeng <mzeng@atlassian.com>
2016-02-01 06:30:53 +1100

    move TodoList to its own file

---
+++ TodoList.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {TodoItem} from "./TodoItem"
+
+export class TodoList extends Component {
+    render() {
+        var createItem = function (item) {
+            return <TodoItem summary={item.summary} key={item.key}/> ;
+        };
+
+        return <ul>{this.props.list.map(createItem)}</ul> ;
+    }
+}
--- entry.js
+++ entry.js
@@ -1,17 +1,7 @@
-import React, {Component} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import data from "./example_data.js";
-import {TodoItem} from "./TodoItem"
+import data from "./example_data";
+import {TodoList} from "./TodoList"
 
-
-class TodoList extends Component {
-    render() {
-            var createItem = function (item) {
-                return <TodoItem summary={item.summary} key={item.key}/> ;
-            };
-
-            return <ul>{this.props.list.map(createItem)}</ul> ;
-    }
-}
 
 ReactDOM.render(<TodoList list={data.items}/>, document.getElementById("app"));
