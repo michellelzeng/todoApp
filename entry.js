51f2c72fc49487b1ab30b4fa40ba878242a06c03
Michelle Zeng <mzeng@atlassian.com>
2016-02-07 17:09:36 +1100

    Add button and show it on the page

--- entry.js
+++ entry.js
@@ -4,6 +4,13 @@ import data from "./example_data";
 import TodoList from "./TodoList"
 
 const init = function() {
+
+    document.getElementById("addTodo").addEventListener("click", function(){
+        const summary = document.getElementById("summary").value;
+        const lastKey = data.items[data.items.length -1].key ;
+        data.items.push({key: lastKey + 1, summary: summary, state: "todo"});
+        ReactDOM.render(<TodoList list={data.items}/>, document.getElementById("todo-list"));
+    });
     ReactDOM.render(<TodoList list={data.items}/>, document.getElementById("todo-list"));
 };
 
--- example_data.js
+++ example_data.js
@@ -1,22 +1,22 @@
 export default {
     "items": [
         {
-            "key": "1",
+            "key": 1,
             "summary": "learn ES6",
             "status": "todo"
         },
         {
-            "key": "2",
+            "key": 2,
             "summary": "learn npm",
             "status": "todo"
         },
         {
-            "key": "3",
+            "key": 3,
             "summary": "learn webpack",
             "status": "todo"
         },
         {
-            "key": "4",
+            "key": 4,
             "summary": "learn react",
             "status": "todo"
         }
