b70c30df8424dcc902532909a41800b152502973
Michelle Zeng <mzeng@atlassian.com>
2016-02-29 08:19:46 +1100

    Add support for multiple cases

--- test/MyStub.js
+++ test/MyStub.js
@@ -1,18 +1,42 @@
+function equal(array1, array2) {
+    return array1.length === array2.length && array1.every((element, index) => element === array2[index]);
+}
+
 const MyStub = (obj, functionName)  => {
     const originalFunction = obj[functionName] ;
-    obj[functionName] = function () {
+    let list = [];
+    obj[functionName] = (a ,b) => {
+        var args = Array.from(arguments);
+        for(let item of list) {
+            if(equal(item.args, args)) {
+                return item.value;
+            }
+        }
     };
 
-    return {
+    const stub = {
       restore: () => {
           obj[functionName] = originalFunction;
+          return stub;
       },
       returns: (value) => {
-          obj[functionName] = () => {
-              return value;
-          };
+          if(list.length == 0) {
+              obj[functionName] = () => {
+                  return value;
+              };
+          }else {
+              //get the last item from the list, and set the item's value to the passed value
+              list[list.length - 1].value = value;
+          }
+          return stub;
+      },
+      calledWith: () => {
+          var args = Array.prototype.slice.call(arguments);
+          list.push({args: args});
+          return stub;
       }
     };
+    return stub;
 };
 
 
--- test/MyStub.test.js
+++ test/MyStub.test.js
@@ -10,13 +10,14 @@ export default function test() {
     let stub = MyStub(obj, 'hello');
     alert(obj.hello());
 
-    stub.restore();
-    alert(obj.hello());
+    //stub.restore();
+    //alert(obj.hello());
+    //
+    //stub.returns('a');
+    //alert(obj.hello());
 
-    stub.returns('a');
-    alert(obj.hello());
+    stub.calledWith(1, 2).returns(3).calledWith(1, 2, 3).returns('explosion');
 
-    stub.calledWith(1, 2).returns(3);
     alert(obj.hello());
 }
 
