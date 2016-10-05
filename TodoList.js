ddbd50992ef220ccd5218e7c3f499bede6a531d6
Michelle Zeng <zlqfly@gmail.com>
2016-10-06 07:04:30 +1100

    make the state in reducer immutable

--- TodoList.js
+++ TodoList.js
@@ -3,15 +3,11 @@ import TodoItem from "./TodoItem"
 
 export default class TodoList extends Component {
     render() {
-//        console.log(this.props.list.toObject());
         var createItem = function (item) {
-//            console.log(item);
             return <TodoItem summary={item.summary} key={item.key}/> ;
         };
 
-        return <div>{this.props.list.toArray().map(createItem)}</div> ;
+        return <div>{this.props.list.map(createItem)}</div> ;
     }
-}
-//TodoList.propTypes = {
-//    list: React.PropTypes.Array
-//}
+ }
+
--- reducer.js
+++ reducer.js
@@ -1,30 +1,39 @@
 import {ADD_TODO, COMPLETE_TODO, addTodo, completeTodo} from './action'
 import data from "./example_data";
-import {List} from 'immutable';
+import Immutable from 'seamless-immutable';
 
-const initialState = {
-    todos: List(data.todos),
+const initialState = Immutable({
+    todos: data.todos,
     summary: ""
-}
+});
 
-console.log(initialState.todos);
-console.log(initialState.todos.toArray());
 function todoApp(state = initialState, action = ADD_TODO) {
     switch(action.type) {
         case ADD_TODO:
+            console.log(state);
             const newId = state.todos[state.todos.length -1].id + 1;
-            return {
-                ...state,
-                todos: state.todos.concat(
-                    {
-                        id: newId,
-                        key: newId,
-                        summary: action.summary,
-                        completed: false
-                    }
-                ),
-                sumamry:""
-            };
+            const newState = state.update('todos', x => (x.concat(
+            {
+                id: newId,
+                key: newId,
+                summary: action.summary,
+                completed: false
+            }
+            )));
+            console.log(newState.todos);
+            return newState;
+//            return {
+//                ...state,
+////                todos: state.todos.concat(
+////                    {
+////                        id: newId,
+////                        key: newId,
+////                        summary: action.summary,
+////                        completed: false
+////                    }
+////                ),
+//                sumamry:""
+//            };
 
         case COMPLETE_TODO:
 
