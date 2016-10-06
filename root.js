ea94411ec096ac51e6cfc87f8a168fd3012d7a5d
Michelle Zeng <mzeng@atlassian.com>
2016-10-07 09:48:53 +1100

    add root component

---
+++ root.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import Issue from './Issue';
+import TodoList from './TodoList';
+
+class Root extends Component {
+    componentDidMount(){
+        this.props.store.subscribe(
+            () => {
+                this.setState(this.props.store.getState());
+            }
+        )
+    }
+    
+    render() {
+        return (
+            <Provider store={this.props.store}>
+                <div>
+                    <TodoList list={this.props.store.getState().todos}/>
+                    {
+                        this.props.store.getState().issues.map((issue, index) => {
+                            return <Issue issueId={issue.id} key={index}/>
+                        })
+                    }
+                </div>
+            </Provider>
+        )
+    }
+}
+
+export default Root;
